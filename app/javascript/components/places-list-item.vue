d04c5374d0ecc02f20cc85837e4ebe182382ccc4
Gonçalo Morais <goncalo@bridge-u.com>
2017-11-20 09:43:37 +0000

    Remove function restored!

--- app/javascript/components/places-list-item.vue
+++ app/javascript/components/places-list-item.vue
@@ -1,19 +1,17 @@
 <template>
-<div>
-  <tr>
-    <td :data-id="place.id" :data-place-id="place.place_id">
-      <div class="row">
-        <div class="nine columns">
-          <div><strong>{{ place.name }}</strong></div>
-          <div>{{ place.address }}</div>
-        </div>
-        <div class="two columns">
-          <button v-on:click="remove(place)">Remove</button>
-        </div>
+<tr>
+  <td :data-id="place.id" :data-place-id="place.place_id">
+    <div class="row">
+      <div class="nine columns">
+        <div><strong>{{ place.name }}</strong></div>
+        <div>{{ place.address }}</div>
       </div>
-    </td>
-  </tr>
-</div>
+      <div class="two columns">
+        <button v-on:click="remove(place)">Remove</button>
+      </div>
+    </div>
+  </td>
+</tr>
 </template>
 
 <script>
@@ -21,18 +19,13 @@ export default {
   props: ['place', 'map'],
   methods: {
     remove: function (place) {
-      this.$http.delete('/pins/' + place.id).then(response => {
-        //var data = JSON.parse(response.body);
-        //console.log('remove successful:', data);
-
-        console.log('remove successful:', place);
-        this.$emit('remove', place);
-      }, response => {
-        var data = JSON.parse(response.body);
-
-        // TODO error callback
-        console.log('did not remove:', response);
-      });
+      this.$http.delete('/pins/' + place.id)
+        .then(_success => {
+          this.$emit('removePlace', place);
+        }, failure => {
+          // TODO error callback
+          console.log('did not remove:', failure);
+        });
     },
   },
 };
