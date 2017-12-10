0d59832206b71dc064263237099a1e4204be402f
Gonçalo Morais <385232+gnclmorais@users.noreply.github.com>
2017-12-10 20:03:14 +0000

    Remove gon gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :google_maps_api_key
-
-  private
-
-  def google_maps_api_key
-    gon.google_maps_api_key = ENV['GOOGLE_MAPS_API_KEY']
-  end
 end
