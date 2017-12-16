317f258f5d71e6efeeb4280e3b380d1685dc598c
Gonçalo Morais <goncalo@bridge-u.com>
2017-12-16 17:09:22 +0000

    Better testing setup

--- test/factories/map.rb
+++ test/factories/map.rb
@@ -3,4 +3,10 @@ FactoryBot.define do
     name 'Get your caffeine fix'
     description 'All the best coffee shops in New York'
   end
+
+  trait :with_one_pin do
+    after(:create) do |map, _evaluator|
+      create :pin, map: map
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_bot_rails'
 
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
