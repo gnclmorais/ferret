5dcb718bcc8333943836356f5573f0d448e66e47
Gonçalo Morais <goncalo@bridge-u.com>
2017-12-16 16:55:07 +0000

    Add FactoryBot to help with future tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,16 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'factory_bot_rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include FactoryBot::Syntax::Methods
+
+  # Useful methods
+  def assert_valid?(object)
+    assert(object.valid?) { object.errors.full_messages.join(" & ") }
+  end
 end
