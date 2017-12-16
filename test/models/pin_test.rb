6437deb88ef9a7b59ae416776a077f80f26b650e
Gonçalo Morais <goncalo@bridge-u.com>
2017-12-16 16:56:27 +0000

    User the new factories

--- test/models/pin_test.rb
+++ test/models/pin_test.rb
@@ -1,22 +1,17 @@
 require 'test_helper'
 
 class PinTest < ActiveSupport::TestCase
-  setup do
-    @map = Map.new(name: 'The Best', description: 'The best of the best.')
-    @place = Place.new(name: 'Dao', description: 'Coffee!', address: 'The Mall')
-  end
-
   test 'has a valid factory' do
     assert_valid? build(:pin)
   end
 
   test 'pin requires a place' do
-    pin = Pin.new(map: @map)
+    pin = build(:pin, place: nil)
     refute pin.valid?
   end
 
   test 'pin requires a map' do
-    pin = Pin.new(place: @place)
+    pin = build(:pin, map: nil)
     refute pin.valid?
   end
 end
