f703e8e8733308303f6b37ea15e91fb8f9e87fe4
Gonçalo Morais <goncalo@bridge-u.com>
2018-01-20 17:37:10 +0000

    Remove mandatory place description

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,6 @@ class Place < ApplicationRecord
   has_many :pins
   has_many :maps, through: :pins
 
-  validates :name,        presence: true
-  validates :description, presence: true
-  validates :address,     presence: true
+  validates :name,    presence: true
+  validates :address, presence: true
 end
