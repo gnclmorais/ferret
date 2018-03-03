48fc010fcd4fbff6769e8880487ace00f48e2be2
Gonçalo Morais <goncalo@bridge-u.com>
2018-03-03 14:20:34 +0000

    Move factories to RSpec

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory(:place) do
+  factory :place do
     name 'Happy Bones'
     sequence(:address, 1) { |n| "#{n} Example Street, New York, NY 10013, USA" }
     google_place_id 'random_google_place_id'
