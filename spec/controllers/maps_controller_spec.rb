b94c8875c741a123241230de639591631f81643d
Gonçalo Morais <goncalo@bridge-u.com>
2018-03-03 16:54:55 +0000

    Better tests for Maps controller

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -4,58 +4,94 @@ RSpec.describe MapsController, type: :controller do
   let(:user) { create(:user) }
   let(:map) { create(:map) }
 
-  context 'Anonymous users' do
-    it '#index is successful' do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe 'Anonymous users' do
+    context 'have read access' do
+      it 'shows the list of maps' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'shows a single map' do
+        get :show, params: { id: map }
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it '#show is successful' do
-      get :show, params: { id: map }
-      expect(response).to have_http_status(:success)
+    context 'do not have write access' do
+      it 'does not show a new map page' do
+        get :new
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'does not edit an existing map' do
+        get :edit, params: { id: map }
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'does not create a new map' do
+        expect do
+          post :create, params: {
+            map: { name: 'Name', description: 'Description' }
+          }
+        end.to_not(change { Map.count })
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'does not update an existing map' do
+        patch :update, params: { id: map, map: { name: 'New Name' } }
+        expect(map.reload.name).to_not eq('New Name')
+      end
+
+      it 'does not destroy an existing map' do
+        delete :destroy, params: { id: map }
+        expect { Map.find(map.id) }.to_not raise_error
+      end
     end
   end
 
-  context 'Logged in users' do
-    before { sign_in_as user }
+  describe 'Logged in users' do
+    context 'have read access' do
+      it 'shows the list of maps' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
 
-    it '#index is successful' do
-      get :index
-      expect(response).to have_http_status(:success)
+      it 'shows a single map' do
+        get :show, params: { id: map }
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it '#show is successful' do
-      get :show, params: { id: map }
-      expect(response).to have_http_status(:success)
-    end
+    context 'have write access' do
+      before { sign_in_as user }
 
-    it '#new is successful' do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+      it 'loads the page for a new map' do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
 
-    it '#edit is successful' do
-      get :edit, params: { id: map }
-      expect(response).to have_http_status(:success)
-    end
+      it 'edits an existing map' do
+        get :edit, params: { id: map }
+        expect(response).to have_http_status(:success)
+      end
 
-    it '#create is successful' do
-      expect do
-        post :create, params: {
-          id: map,
-          map: { name: 'Name', description: 'Description' }
-        }
-      end.to(change { Map.count })
-    end
+      it 'creates a new map' do
+        expect do
+          post :create, params: {
+            map: { name: 'Name', description: 'Description' }
+          }
+        end.to(change { Map.count })
+      end
 
-    it '#update is successful' do
-      patch :update, params: { id: map, map: { name: 'New Name' } }
-      expect(map.reload.name).to eq('New Name')
-    end
+      it 'updates an existing map' do
+        patch :update, params: { id: map, map: { name: 'New Name' } }
+        expect(map.reload.name).to eq('New Name')
+      end
 
-    it '#destroy is successful' do
-      delete :destroy, params: { id: map }
-      expect { Map.find(map.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      it 'destroys an existing map' do
+        delete :destroy, params: { id: map }
+        expect { Map.find(map.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
