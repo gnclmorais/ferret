84d7be7ee9499dc2513d3900c909ae5d3bb425de
Gonçalo Morais <goncalo@bridge-u.com>
2018-03-24 18:17:48 +0000

    Rewrite the migration and allow tag delete

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   resources :maps
   resources :pins, only: %i[create destroy]
   resources :search_places, only: %i[create]
-  resources :tagged_pins, only: %i[create]
+  resources :tagged_pins, only: %i[create destroy]
 end
