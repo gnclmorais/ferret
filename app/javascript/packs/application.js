e1881a550559a8bc6b2d331ec1fda922fc137648
Gonçalo Morais <goncalo@bridge-u.com>
2018-03-27 10:02:49 +0100

    Bring jQuery and jQuery UJS in another way

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import $ from 'jquery'
-import {} from 'jquery-ujs'
-
 import '../../stylesheets/bulma.scss';
 import 'font-awesome-webpack'
 
