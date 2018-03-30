70eb8eedda23614ccc3dcf28ccaa3e311d29d2c6
Gonçalo Morais <goncalo@bridge-u.com>
2018-03-30 16:21:21 +0100

    Add page refresh helper

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  def refresh
+    page.driver.browser.navigate.refresh
+  end
 end
