5d43c105562e3916dacfacd9ff00b9efccc5eae0
Gonçalo Morais <goncalo@bridge-u.com>
2018-04-01 20:17:59 +0100

    Fix some system tests

--- test/system/tags_test.rb
+++ test/system/tags_test.rb
@@ -4,28 +4,37 @@ class TagsTest < ApplicationSystemTestCase
   setup do
     @user = create(:user)
     @map = create(:map, owner: @user)
-
-    # sign_in_as(@user)
-    # sign_in_with(@user.email, 'password')
   end
 
   test 'map owner can add a tag' do
     create(:pin, map: @map)
-
-    visit root_path(as: @user)
     visit edit_map_path(@map, as: @user)
-    assert_no_text 'coffee'
+    assert_no_text 'v60'
 
-    click_link '+ add tag'
-    tag_input = find('input[type="text"]')
-    tag_input.set('coffee')
-    tag_input.send_keys(:enter)
+    within '#places-panel' do
+      click_button '+ add tag'
+      tag_input = first('input[type="text"]')
+      tag_input.set('v60')
+      tag_input.send_keys(:enter)
+    end
 
     refresh
-    assert_text 'coffee'
+    assert_text 'v60'
   end
 
   test 'map owner can delete a tag' do
+    pin = create(:pin, map: @map)
+    create(:tagged_pin, pin: pin, tag: create(:tag, name: 'espresso'))
+    binding.pry
+    visit edit_map_path(@map, as: @user)
+    assert_text 'espresso'
+
+    within '#places-panel' do
+      find('.is-delete').click
+    end
+
+    refresh
+    assert_no_text 'espresso'
   end
 
   test 'other users see tags but no delete button' do
