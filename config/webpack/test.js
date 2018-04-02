15bfed4cfd3cd2c3be18202237ac57387ea734ee
Gonçalo Morais <goncalo@bridge-u.com>
2018-04-02 19:39:19 +0100

    WEbpack config

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,10 +2,4 @@ const merge = require('webpack-merge')
 const environment = require('./environment')
 const customConfig = require('./custom')
 
-module.exports = merge({
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.js',
-    },
-  },
-}, environment.toWebpackConfig(), customConfig)
+module.exports = merge({}, environment.toWebpackConfig(), customConfig)
