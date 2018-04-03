aea9872229d7d280fabb731006c5e64194f85cad
Gonçalo Morais <goncalo@bridge-u.com>
2018-04-03 08:37:38 +0100

    Try a few different Capybara config flags

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,19 @@
-require "test_helper"
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  Capybara.register_driver :chrome do |app|
+    browser_options = ::Selenium::WebDriver::Chrome::Options.new
+    browser_options.args << '--headless'
+    # browser_options.args << '--disable-gpu'
+    # browser_options.args << '--window-size=1920,2000'
+    # browser_options.args << '--no-sandbox' if ENV['CONTINUOUS_INTEGRATION']
+    Capybara::Selenium::Driver.new(
+      app, browser: :chrome, options: browser_options
+    )
+  end
+  Capybara.javascript_driver = :chrome
 
   def refresh
     page.driver.browser.navigate.refresh
