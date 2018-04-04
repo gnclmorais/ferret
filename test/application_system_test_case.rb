546cde7eb6afb5759ff11a786dd6ed51b71f9b1d
Gonçalo Morais <goncalo@bridge-u.com>
2018-04-04 09:34:13 +0100

    Try headless Firefox for system testing

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 require 'capybara'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :headless_firefox
+
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
   # Capybara.register_driver :chrome do |app|
@@ -18,18 +20,18 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # Capybara.javascript_driver = :chrome
 
   # From https://github.com/rails/rails/issues/29688
-  Capybara.register_driver(:headless_chrome) do |app|
-    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless disable-gpu window-size=1920x1080] }
-    )
+  # Capybara.register_driver(:headless_chrome) do |app|
+  #   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+  #     chromeOptions: { args: %w[headless disable-gpu window-size=1920x1080] }
+  #   )
 
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      desired_capabilities: capabilities
-    )
-  end
-  driven_by :headless_chrome
+  #   Capybara::Selenium::Driver.new(
+  #     app,
+  #     browser: :chrome,
+  #     desired_capabilities: capabilities
+  #   )
+  # end
+  # driven_by :headless_chrome
 
   def refresh
     page.driver.browser.navigate.refresh
