dc6e0a9b9a3407761c707dbd2f4cc51e5e0fe1a5
Gonçalo Morais <goncalo@bridge-u.com>
2018-04-04 09:17:33 +0100

    Try a different driver
    From https://github.com/rails/rails/issues/29688

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,20 +1,35 @@
 require 'test_helper'
+require 'capybara'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
-  Capybara.register_driver :chrome do |app|
-    browser_options = ::Selenium::WebDriver::Chrome::Options.new
-    browser_options.args << '--headless'
-    browser_options.args << '--no-sandbox' if ENV['CONTINUOUS_INTEGRATION']
-    browser_options.args << '--disable-gpu'
-    # browser_options.args << '--window-size=1920,2000'
-    # browser_options.args '--remote-debugging-port=9222'
+  # Capybara.register_driver :chrome do |app|
+  #   browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  #   browser_options.args << '--headless'
+  #   browser_options.args << '--no-sandbox' if ENV['CONTINUOUS_INTEGRATION']
+  #   browser_options.args << '--disable-gpu'
+  #   # browser_options.args << '--window-size=1920,2000'
+  #   # browser_options.args '--remote-debugging-port=9222'
+  #   Capybara::Selenium::Driver.new(
+  #     app, browser: :chrome, options: browser_options
+  #   )
+  # end
+  # Capybara.javascript_driver = :chrome
+
+  # From https://github.com/rails/rails/issues/29688
+  Capybara.register_driver(:headless_chrome) do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w[headless disable-gpu window-size=1920x1080] }
+    )
+
     Capybara::Selenium::Driver.new(
-      app, browser: :chrome, options: browser_options
+      app,
+      browser: :chrome,
+      desired_capabilities: capabilities
     )
   end
-  Capybara.javascript_driver = :chrome
+  driven_by :headless_chrome
 
   def refresh
     page.driver.browser.navigate.refresh
