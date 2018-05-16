cf09ec6c1099dcd53189036ed712487ada8c07aa
Gonçalo Morais <goncalo@bridge-u.com>
2018-05-16 19:01:21 -0400

    Skip homepage if signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
+  before_action :skip_homepage, only: %w[index]
+
   def index; end
+
+  private
+
+  def skip_homepage
+    redirect_to maps_path if signed_in?
+  end
 end
