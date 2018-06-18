16f8b56383d0cba239345670aee6010833275fc2
Gonçalo Morais <goncalo@bridge-u.com>
2018-06-18 19:38:02 +0100

    Improve migration

--- db/migrate/20180618182158_rename_private_to_published_on_maps.rb
+++ db/migrate/20180618182158_rename_private_to_published_on_maps.rb
@@ -1,5 +1,13 @@
 class RenamePrivateToPublishedOnMaps < ActiveRecord::Migration[5.1]
-  def change
+  def up
     rename_column :maps, :private, :published
+
+    Map.update_all(published: true)
+  end
+
+  def down
+    rename_column :maps, :published, :private
+
+    Map.update_all(private: false)
   end
 end
