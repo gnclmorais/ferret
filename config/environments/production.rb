e7ff3a49d3ff237e1079db4d3d616e3a5ebf6214
Gonçalo Morais <goncalo@bridge-u.com>
2018-11-04 22:54:25 +0000

    Upgrade to Rails 5.2
    More info at https://guides.rubyonrails.org/5_2_release_notes.html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -61,11 +65,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
-  # Settings
-  config.action_mailer.default_url_options = {
-    host: 'mapped-out-tv.herokuapp.com'
-  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
