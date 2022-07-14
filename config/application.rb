deed5798ddd21bca27068453b80ca3b6561bb8d2
Gonçalo Morais <385232+gnclmorais@users.noreply.github.com>
2022-07-14 18:02:29 +0200

    Update Rails to 6.1

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,8 @@ module Ferret
     config.x.contact_email = 'bingewatchingcollection@gmail.com'
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
+    config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
